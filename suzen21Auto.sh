a83ae2ab3748199eef4c2449c210fd511ac8d3fb
DTaSchweppes <45369246+DTaSchweppes@users.noreply.github.com>
2019-05-26 22:14:27 +0300

    Update Suzen21Auto
    redid the logic

--- suzen21Auto.sh
+++ suzen21Auto.sh
@@ -1,8 +1,5 @@
 #!bin/bash
 find . -type f | wc -l #check count files
-rm [0-9].txt
-rm test-*.log
-rm [a-z].png
-rm [0-9][0-9].txt
-ls
+rm [0-9].txt && rm [a-z].png && rm test-*.log
+echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
 
